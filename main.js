28cc61b76801181bb6bdd2d16d3e77b1587a09f1
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2019-11-09 11:52:15 +0900

    add show latitude and longtitude

--- main.js
+++ main.js
@@ -4,4 +4,20 @@ function initMap() {
     center: new google.maps.LatLng(35.681236,139.767125)
   };
   var map = new google.maps.Map(document.getElementById("map"), opts);
+
+  map.addListener('click', function(e) {
+    getLatLng(e.latLng, map)
+  });
+}
+
+function getLatLng(latLng, map) {
+  document.getElementById('lat').textContent = latLng.lat();
+  document.getElementById('lng').textContent = latLng.lng();
+
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map
+  });
+
+  map.panTo(latLng);
 }
